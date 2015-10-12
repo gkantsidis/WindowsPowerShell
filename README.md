c84a3558549110e183a6a4c9d02cd75609c509f7
Christos Gkantsidis <gkantsidis@gmail.com>
2015-10-13 00:48:43 +0100

    very simple profile with a simple function that opens files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WindowsPowerShell
 Windows Powershell Profile
+
+Some general instructions for creating Windows PowerShell profiles:
+https://technet.microsoft.com/en-us/library/hh847857.aspx
---
+++ profile.ps1
@@ -0,0 +1,64 @@
+<#
+.SYNOPSIS
+Opens a file with an appropriate editor.
+
+.DESCRIPTION
+The Get-FileInEditor attempts to open a file using an appropriate editor. It will guess the editor based on suffix
+and various heuristics. For example, LaTeX related files open with emacs; solution and project files with VS.
+
+
+.PARAMETER Filename
+The name of the file to open.
+
+
+.PARAMETER NoWait
+Do not wait the editor to finish with the file before continuing.
+
+
+.EXAMPLE
+
+Create a new file, or open the existing main.tex file.
+Get-FileInEditor main.tex
+
+#>
+function Get-FileInEditor {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Filename,
+
+        [Switch]
+        $NoWait
+	)
+
+	if ([System.String]::IsNullOrWhiteSpace($Filename)) {
+        Write-Error "Name of input file cannot be null or empty."
+        throw [System.ArgumentNullException] "Input file has null or empty name."
+    }
+
+    if (Test-Path -Path $Filename -PathType Container) {
+        explorer $Filename
+    } else {
+        $o = New-Object -TypeName System.IO.FileInfo -ArgumentList $Filename
+
+        if ($NoWait) {
+            $emacsNoWait = '-n'
+        } else {
+            $emacsNoWait = ''
+        }
+
+        if ($o.Extension -in '.tex', '.bib') {
+            emacsclient $emacsNoWait $o
+        } elseif ($o.Extension -in '.c', '.h', '.cpp', '.hpp') {
+            emacsclient $emacsNoWait $o
+        } elseof ($o.Extension -in '.tab', '.csv', '.txt') {
+            emacsclient $emacsNoWait $o
+        } else {
+            notepad $o
+        }
+    }
+}
+
+Set-Alias -Name e -Value Get-FileInEditor
