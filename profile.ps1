a73494a3b8578bb6239893194fa2403cfd6b637a
Christos Gkantsidis <gkantsidis@gmail.com>
2015-10-13 09:24:01 +0100

    old modules start properly

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,36 @@
+#
+# Import external modules
+# 
+
+$private:PowerShellProfileDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent 
+Push-Location $private:PowerShellProfileDirectory
+
+. 'Modules\posh-git\profile.example.ps1'
+
+Rename-Item Function:\Prompt PoshGitPrompt -Force
+function Prompt() {
+    if (Test-Path Function:\PrePoshGitPrompt) {
+        ++$global:poshScope
+        New-Item function:\script:Write-host -value "param([object] `$object, `$backgroundColor, `$foregroundColor, [switch] `$nonewline) " -Force | Out-Null
+        $private:p = PrePoshGitPrompt
+        if(--$global:poshScope -eq 0) {
+            Remove-Item function:\Write-Host -Force
+        }
+    }
+    PoshGitPrompt
+}
+
+Invoke-Expression -Command .\Modules\Posh-GitHub\Posh-GitHub-Profile.ps1
+Invoke-Expression -Command .\Modules\Posh-VsVars\Posh-VsVars-Profile.ps1
+
+Import-Module Invoke-MSBuild
+
+Pop-Location
+
+#
+# Local Functions
+# 
+
 <#
 .SYNOPSIS
 Opens a file with an appropriate editor.
@@ -31,9 +64,9 @@ function Get-FileInEditor {
 
         [Switch]
         $NoWait
-	)
+    )
 
-	if ([System.String]::IsNullOrWhiteSpace($Filename)) {
+    if ([System.String]::IsNullOrWhiteSpace($Filename)) {
         Write-Error "Name of input file cannot be null or empty."
         throw [System.ArgumentNullException] "Input file has null or empty name."
     }
