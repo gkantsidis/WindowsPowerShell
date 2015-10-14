19eef239c651f58997b22d3759d439b7a4461698
Christos Gkantsidis <gkantsidis@gmail.com>
2015-10-14 13:49:10 +0100

    testing for VS

--- profile.ps1
+++ profile.ps1
@@ -1,6 +1,10 @@
 $private:PowerShellProfileDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent 
 Push-Location $private:PowerShellProfileDirectory
 
-. .\profile_desktop.ps1
-
+if ($Env:VSIDE) {
+    # Inside Visual Studio --- ignore all initializations
+} else {
+    # Regular (desktop) mode
+    . .\profile_desktop.ps1
+}
 Pop-Location
