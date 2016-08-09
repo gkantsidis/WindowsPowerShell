fd259c8c2bcffc9204645a7bc68c7ee58a53f87d
Christos Gkantsidis <chrisgk@microsoft.com>
2016-08-09 12:00:03 +0100

    fixed location of modules for Powershell ISE

--- Microsoft.PowerShellISE_profile.ps1
+++ Microsoft.PowerShellISE_profile.ps1
@@ -1,4 +1,12 @@
+# $git = Get-command -Name git
+# $gitDir = Get-Item -Path $git.Path
+# $folder = Join-Path -Path $gitDir.Directory.FullName -ChildPath ".." | `
+          # Join-Path -ChildPath "usr" | `
+          # Join-Path -ChildPath "bin"
+
+# $Env:Path = $Env:Path + ";$folder"
+
 #####ISEPESTER#####
-Import-Module C:\ProgramData\chocolatey\lib\IsePester\tools\IsePester.psm1
-Import-Module C:\ProgramData\chocolatey\lib\pester\tools\Pester.psm1
+Import-Module $Env:ChocolateyInstall\lib\IsePester\tools\IsePester.psm1
+Import-Module $Env:ChocolateyInstall\lib\pester\tools\Pester.psm1
 #####ISEPESTER#####
