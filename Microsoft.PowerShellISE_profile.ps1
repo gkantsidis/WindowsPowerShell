367837b554d4d0a5cfd9a80b1fc91fdc47af31f9
Christos Gkantsidis <gkantsidis@gmail.com>
2016-09-30 23:33:09 +0100

    removing from depot

--- Microsoft.PowerShellISE_profile.ps1
+++
@@ -1,6 +0,0 @@
-#####ISEPESTER#####
-# Import-Module $Env:ChocolateyInstall\lib\IsePester\tools\IsePester.psm1
-# Import-Module $Env:ChocolateyInstall\lib\pester\tools\Pester.psm1
-Import-Module Pester
-Import-Module IsePester
-#####ISEPESTER#####
