75cd124408693f5221a4ce1b02c16c129b191768
Christos Gkantsidis <gkantsidis@gmail.com>
2016-11-05 11:41:48 +0000

    alternative prompt

---
+++ Prompts.ps1
@@ -0,0 +1,17 @@
+function global:prompt {
+    $maxPath = 20
+    $drive = $pwd.Drive.Name
+    $path = $pwd.Path.Substring($drive.Length + 1)
+    $adjusted = $false
+    $lastIndex = $path.Length - $maxPath
+    if ($lastIndex -gt 0) {
+        $lastIndex = $path.LastIndexOf("\", $lastIndex - 1)
+        $adjusted = $true
+    }
+    if ($adjusted) {
+        $path = "..." + $path.Substring($lastIndex)
+    }
+    $p = $drive + ":" + $path + " ë"
+    Write-Host $p -NoNewLine -ForegroundColor "DarkGray"
+    return " "
+}
