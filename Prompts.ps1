6a712a4246ed38fd650f195deee4e392115e3a82
Christos Gkantsidis <gkantsidis@gmail.com>
2016-11-25 08:52:46 +0000

    added command to change type of prompt

--- Prompts.ps1
+++ Prompts.ps1
@@ -15,3 +15,34 @@
     Write-Host $p -NoNewLine -ForegroundColor "DarkGray"
     return " "
 }
+
+function global:Set-NormalPrompt {
+    function global:prompt {
+        $maxPath = 20
+        $drive = $pwd.Drive.Name
+        $path = $pwd.Path.Substring($drive.Length + 1)
+        $adjusted = $false
+        $lastIndex = $path.Length - $maxPath
+        if ($lastIndex -gt 0) {
+            $lastIndex = $path.LastIndexOf("\", $lastIndex - 1)
+            $adjusted = $true
+        }
+        if ($adjusted) {
+            $path = "..." + $path.Substring($lastIndex)
+        }
+        $p = $drive + ":" + $path + " λ"
+        Write-Host $p -NoNewLine -ForegroundColor "DarkGray"
+        return " "
+    }
+}
+
+function global:Set-GitPrompt {
+    function global:prompt {
+        $realLASTEXITCODE = $LASTEXITCODE
+        Write-Host($pwd.ProviderPath) -nonewline
+        Write-VcsStatus
+        $global:LASTEXITCODE = $realLASTEXITCODE
+        Write-Host "`nλ" -NoNewLine -ForegroundColor "DarkGray"
+        return " "
+    }
+}
