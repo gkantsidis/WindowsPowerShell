a48c006ef12a76e4b56629f295fbcd92347caa50
Christos Gkantsidis <chrisgk@microsoft.com>
2017-09-20 13:25:38 +0100

    better parameter passing

--- Modules/Environment/Directories.ps1
+++ Modules/Environment/Directories.ps1
@@ -1,31 +1,40 @@
 # Some shortcuts that make life easier
 
-function Get-ChildItemByPage { Get-ChildItem $args | more }
+function Get-ChildItemByPage { Get-ChildItem @args | more }
 
-function Get-ChildItemWithOwner { Get-ChildItem $args | Get-Acl | Select-Object Path,Owner }
+function Get-ChildItemWithOwner { Get-ChildItem @args | Get-Acl | Select-Object Path,Owner }
 
 function Get-ChildItemWithStreams {
-    $result = Get-ChildItem $args
-    [object[]]$everything = $result | ForEach-Object -Process {
-        $_
+    Get-ChildItem @args | ForEach-Object -Process {
+        Write-Output -InputObject $_
         $mystreams = Get-Item $_.FullName -Stream * -ErrorAction SilentlyContinue | `
                      Where-Object -Property Stream -NE -Value ":`$DATA"
-        $mystreams
+        Write-Output -InputObject $mystreams
     }
-    $everything
 }
 
 function Get-ChildItemWithHidden {
-    [object[]]$visible = Get-ChildItem $args
-    [object[]]$hidden = Get-ChildItem -Hidden $args
-    $result = $visible + $hidden
-    [object[]]$everything = $result | ForEach-Object -Process {
-        $_
-        $mystreams = Get-Item $_.FullName -Stream * -ErrorAction SilentlyContinue | `
-                     Where-Object -Property Stream -NE -Value ":`$DATA"
-        $mystreams
+    # There is redundancy in the code below, but this way we get the output
+    # pipeline working better.
+
+    Get-ChildItem @args | ForEach-Object -Process {
+        $item = $_
+        Write-Output -InputObject $item
+        if (Get-Member -InputObject $item -Name FullName -ErrorAction SilentlyContinue) {
+            $mystreams = Get-Item $item.FullName -Stream * -ErrorAction SilentlyContinue | `
+                        Where-Object -Property Stream -NE -Value ":`$DATA"
+            Write-Output -InputObject $mystreams
+        }
     }
-    $everything
+    Get-ChildItem -Hidden @args | ForEach-Object -Process {
+        $item = $_
+        Write-Output -InputObject $item
+        if (Get-Member -InputObject $item -Name FullName -ErrorAction SilentlyContinue) {
+            $mystreams = Get-Item $item.FullName -Stream * -ErrorAction SilentlyContinue | `
+                        Where-Object -Property Stream -NE -Value ":`$DATA"
+            Write-Output -InputObject $mystreams
+        }
+    }
 }
 
 Set-Alias -Name "dir/a" -Value Get-ChildItemWithHidden
