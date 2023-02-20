18c4a8f642c1a8b46d41abd3ce0e886789bc90a4
Christos Gkantsidis <chrisgk@microsoft.com>
2023-02-20 12:48:02 +0000

    autocomplete for rig where available

--- Microsoft.PowerShell_Win32NT_profile.ps1
+++ Microsoft.PowerShell_Win32NT_profile.ps1
@@ -3,3 +3,9 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+if (Get-Command -Name scoop -ErrorAction SilentlyContinue) {
+  # Scoop  - rig autocompletion
+  $rig_ac=$(try { Join-Path -Path $(scoop prefix rig) -ChildPath _rig.ps1 } catch { '' })
+  if (Test-Path -Path $rig_ac)  { & $rig_ac }
+}
